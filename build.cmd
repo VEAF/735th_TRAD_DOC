d6099d288d1011ebd26853a364d179579da94efb
Couby <couby40@gmail.com>
2019-12-26 07:36:49 +0100

    Small update. Build.cmd added.

---
+++ build.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo -
+echo -----------------------------------------------------
+echo  Building Training Air Defense Mission Documentation
+echo -----------------------------------------------------
+
+echo Generate documentation
+hugo -d docs
+
+echo _________________________
+echo  Documentation generated
